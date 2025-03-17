d1dfe77d5d8fee3e6c6557651336be2868bb8df4
Antonio Ojea <aojea@google.com>
2025-03-17 10:30:41 +0000

    allow to use macvlan interfaces
    Change-Id: I04f5b1f54916eebb6ef5b2c24a2fb6dec030e0ea

---
+++ pkg/driver/subinterfaces.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2024 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package driver
+
+import (
+	"fmt"
+
+	"github.com/vishvananda/netlink"
+	"github.com/vishvananda/netns"
+)
+
+func addMacVlan(containerNsPAth string, devName string) error {
+	containerNs, err := netns.GetFromPath(containerNsPAth)
+	if err != nil {
+		return fmt.Errorf("could not get network namespace from path %s for network device %s : %w", containerNsPAth, devName, err)
+	}
+	parentLink, err := netlink.LinkByName(devName)
+	if err != nil {
+		return fmt.Errorf("could not find parent interface %s : %w", devName, err)
+	}
+	macvlan := &netlink.Macvlan{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:        "knet-" + devName,
+			ParentIndex: parentLink.Attrs().Index,
+			NetNsID:     int(containerNs),
+		},
+		Mode: netlink.MACVLAN_MODE_BRIDGE,
+	}
+	if err := netlink.LinkAdd(macvlan); err != nil {
+		// If a user creates a macvlan and ipvlan on same parent, only one slave iface can be active at a time.
+		return fmt.Errorf("failed to create the %s macvlan interface: %v", macvlan.Name, err)
+	}
+
+	return nil
+}
